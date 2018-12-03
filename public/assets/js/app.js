68cb775fb1531f05ec6d9120ceaadef8deca9e24
AlanEc <alan.ecalle@laposte.net>
2018-12-03 09:56:08 +0100

    Creation NavBar

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,15 @@
 // app.js
 
-const $ = require('jquery');
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 
+const $ = require('jquery');
 require('../css/app.css');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+require('bootstrap/dist/js/bootstrap');
 
 
 // or you can include specific pieces
