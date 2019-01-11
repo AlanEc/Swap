a36590d429afae1db531c55ef9a9c151edd5bba8
AlanEc <alan.ecalle@laposte.net>
2019-01-12 00:57:41 +0100

    Create MessageFormType

---
+++ src/Form/MessageFormType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: annes
+ * Date: 12/01/2019
+ * Time: 00:55
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Message;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', TextareaType::class, array(
+                'label' => false
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Envoyer'
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class
+        ]);
+    }
+}
+
