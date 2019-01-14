4cfae47ec7be4ce6b5557a25f71d93c72534fffa
AlanEc <alan.ecalle@laposte.net>
2019-01-14 15:23:24 +0100

    timestampable

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -11,6 +11,9 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Booking;
+use App\Entity\SwapService;
+use App\Entity\BookingState;
+use App\Entity\BookingType;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Form\BookingFormType;
@@ -31,15 +34,35 @@ class BookingController extends AbstractController
 
     /**
      * @IsGranted("ROLE_USER")
-     * @Route("/booking/new", name="swap_booking_new")
+     * @Route("/booking/new/{swapId}", name="swap_booking_new")
      */
-    public function new(Request $request)
+    public function new(Request $request, $swapId)
     {
         $form = $this->createForm(BookingFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $booking  = $form->getData();
+            $booking->setDisabled(0);
 
+            $repository = $this->getDoctrine()->getRepository(SwapService::class);
+            $swap = $repository->findOneBy(['id' => $swapId]);
+
+            $repository = $this->getDoctrine()->getRepository(BookingState::class);
+            $bookingState = $repository->findOneBy(['id' => 92]);
+
+            $repository = $this->getDoctrine()->getRepository(BookingType::class);
+            $bookingType = $repository->findOneBy(['id' => 48]);
+
+            $user = $this->getUser();
+
+            $booking->setBookingType($bookingType);
+            $booking->setBookingState($bookingState);
+            $booking->setUser($user);
+            $booking->setSwapService($swap);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($booking);
+            $em->flush();
         }
 
         return $this->render('core/swapService/booking.html.twig', [
--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BookingRepository")
@@ -35,11 +36,13 @@ class Booking
 
     /**
      * @ORM\Column(type="datetime")
+     * @Gedmo\Timestampable(on="create")
      */
     private $created_at;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Gedmo\Timestampable(on="update")
      */
     private $updated_at;
 
